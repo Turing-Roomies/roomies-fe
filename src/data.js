f1d77d61a58e1179427a930fc269ca699316fe30
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-10 14:30:18 -0400

    Create placeholder data file

---
+++ src/data.js
@@ -0,0 +1,94 @@
+export const users = {
+        "data": [{
+            "type": "users",
+            "id": "1",
+            "attributes": {
+                "email": "whatever@example.com",
+                "name": "Harrison",
+                "location": "Denver",
+                "gender": "male",
+                "age": 26,
+                "roomies": [{
+                    "name": "Wyatt",
+                    "location": "Denver",
+                }]
+            }
+        },
+        {
+            "type": "users",
+            "id": "2",
+            "attributes": {
+                "email": "someother@example.com",
+                "name": "Wyatt",
+                "location": "Denver",
+                "gender": "male",
+                "age": 30,
+                "roomies": [
+                    {
+                        "name": "Harrison",
+                        "location": "Denver",
+                    },
+                    {
+                        "name": "Michann",
+                        "location": "Denver"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "users",
+            "id": "3",
+            "attributes": {
+                "email": "emailme@example.com",
+                "name": "Andrew",
+                "location": "West Palm Beach",
+                "gender": "male",
+                "age": 25,
+                "roomies": [
+                    {
+                        "name": "Dustin",
+                        "location": "Orlando",
+                    }
+                ]
+            }
+        },
+        {
+            "type": "users",
+            "id": "4",
+            "attributes": {
+                "email": "email@example.com",
+                "name": "Sarah",
+                "location": "Denver",
+                "gender": "female",
+                "age": 33,
+                "roomies": [
+                    {
+                        "name": "Michann",
+                        "location": "Denver",
+                    },
+                ]
+            }
+        },
+        {
+            "type": "users",
+            "id": "5",
+            "attributes": {
+                "email": "somebodysemail@example.com",
+                "name": "Michann",
+                "location": "Denver",
+                "gender": "female",
+                "age": 27,
+                "roomies": [
+                    {
+                        "name": "Sarah",
+                        "location": "Denver",
+                    },
+                    {
+                        "name": "Wyatt",
+                        "location": "Denver",
+                    }
+                ]
+            }
+        }
+        ]
+    }
