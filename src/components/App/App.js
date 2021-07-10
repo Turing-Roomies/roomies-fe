ba653a826214d49e9dc225c4613f8b7d41a78ee3
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-10 13:18:45 -0400

    Add Home component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
+import Home from '../Home/Home'
 import './App.css'
 
 export default function App() {
-  
-
 
   return (
     <main>
       <Switch>
         <Navbar />
+        <Route 
+        path='/'
+        component={Home}
+        />
       </Switch>
     </main>
   )
