c5a09ccc66c3d5eb38f16f9f66005dd05c008eee
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-10 14:50:12 -0400

    Add dashboard route to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
+import Dashboard from '../Dashboard/Dashboard'
 import './App.css'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/dashboard' component={Dashboard} />
       </Switch>
     </main>
   )
