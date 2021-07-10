0717f102d703b76bb4d3cc7ddc7d986289f8f73f
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-10 15:33:36 -0400

    Wrap nodes in context provider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fakeData } from '../../data'
 import { Switch, Route } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
 import './App.scss'
 import Dashboard from '../Dashboard/Dashboard'
+import Context from '../../Context'
 
 export default function App() {
 
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+      setUsers(fakeData.data)
+  }, [])
+
   return (
-    <main>
-        <Navbar />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/dashboard' component={Dashboard} />
-      </Switch>
-    </main>
+    <Context.Provider value={users}>
+      <main>
+          <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/dashboard' component={Dashboard} />
+        </Switch>
+      </main>
+    </Context.Provider>
   )
 }
