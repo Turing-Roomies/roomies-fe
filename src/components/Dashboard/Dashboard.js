4cc50a03aecb5c9786daa66312c264a8d367ce21
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-10 17:13:31 -0400

    Map over users to pass info as props to cards

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Card from '../Card/Card'
+import Context from '../../Context'
 
 export default function Dashboard() {
+    const users = useContext(Context)
+       const cards = users.map((user) => {
+console.log(user);
+        return <Card  user={user} key={user.id} />
+      })
 
-    return (
-        <h1>Mermaid Man</h1>
-    )
+        return(
+            <div>
+                {cards}
+            </div>
+        )
 }
