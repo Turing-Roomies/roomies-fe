53914a7fb9920f254d857235124e67acf19c8e11
Sarah Lane <sarahlane8@gmail.com>
2021-07-11 06:14:41 -0600

    Add should display the nav bar test

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -3,4 +3,10 @@ describe('Home', () => {
     cy.visit('http://localhost:3000')
   })
 
+  it('Should display the nav bar', () => {
+    cy.get('nav').should('be.visible')
+      .get('nav > a').should('contain', 'Roomies')
+      .get('.nav-links > li').should('have.length', 1)
+      .get('.nav-links > li > a').eq(0).should('contain', 'Dashboard')
+  })
 })
