6b04e1828de805dc38df08e0434bf14df2b388cb
Sarah Lane <sarahlane8@gmail.com>
2021-07-11 06:47:20 -0600

    Complete home cypress tests

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,12 +1,19 @@
 describe('Home', () => {
-  beforeEach( () => {
-    cy.visit('http://localhost:3000')
-  })
+    beforeEach( () => {
+        cy.visit('http://localhost:3000')
+    })
 
-  it('Should display the nav bar', () => {
-    cy.get('nav').should('be.visible')
-      .get('nav > a').should('contain', 'Roomies')
-      .get('.nav-links > li').should('have.length', 1)
-      .get('.nav-links > li > a').eq(0).should('contain', 'Dashboard')
-  })
+    it('Should display the nav bar', () => {
+        cy.get('nav').should('be.visible')
+          .get('nav > a').should('contain', 'Roomies')
+          .get('.nav-links > li').should('have.length', 1)
+          .get('.nav-links > li > a').eq(0).should('contain', 'Dashboard')
+    })
+
+    it('Should display the welcome message and image', () => {
+        cy.get('.welcome-section').should('be.visible')
+          .get('.welcome-container').should('be.visible')
+          .get('.welcome-message').should('contain', 'Find your', 'perfect', ' match')
+    })
+    
 })
