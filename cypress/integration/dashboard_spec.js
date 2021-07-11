df57d915fa3fde12818a4897dd34bf9d0d956adc
Sarah Lane <sarahlane8@gmail.com>
2021-07-11 07:48:57 -0600

    Complete should display the feed with roomies test

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -12,6 +12,13 @@ describe('Dashboard', () => {
 
     it('Should display the feed with possible roomies', () => {
         cy.url().should('eq', 'http://localhost:3000/dashboard')
+          .get('.card').should('have.length', 5)
+
+          .get('.card').eq(0).should('contain', 'Harrison', 'whatever@example.com', 'Denver', 'male', '26')
+          .get('.card').eq(1).should('contain', 'Wyatt', 'someother@example.com', 'Denver', 'male', '30')
+          .get('.card').eq(2).should('contain', 'Andrew', 'emailme@example.com', 'West Palm Beach', 'male', '25')
+          .get('.card').eq(3).should('contain', 'Sarah', 'email@example.com', 'Denver', 'female', '33')
+          .get('.card').eq(4).should('contain', 'Michann', 'somebodysemail@example.com', 'Denver', 'female', '27')
     })
 
 
