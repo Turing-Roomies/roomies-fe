3207991dec8c18da7ef4f03059c520f4d9b3db91
Your Name <stoner.michann@gmail.com>
2021-07-12 16:38:57 -0600

    Create containers and import icons

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Card.scss'
+import locationIcon from '../../assets/location-icon.png'
+import userIcon from '../../assets/user-icon.png'
 
 export default function Card({ user }) {
 const { email, name, location, gender, age } = user.attributes
@@ -12,8 +14,14 @@ console.log(email);
             <h3>{age}</h3>
           </div>
           <div className='card-info'>
-          <p>City: {location}</p>
-          <p>Gender: {gender}</p>
+            <div className='location-container'>
+              <img className='location-icon' src={locationIcon} alt='location icon'/>
+              <p>{location}</p>
+            </div>
+            <div className='gender-container'>
+              <img className='user-icon' src={userIcon} alt='user icon'/>
+              <p>{gender}</p>
+            </div>
           </div>
           <button className='req-contact'>Request Contact</button>
         </article>
