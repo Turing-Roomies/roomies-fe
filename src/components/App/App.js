aee994060dd51db9a9d22f30c7dca45bf6c76be1
Sarah Lane <sarahlane8@gmail.com>
2021-07-12 17:25:49 -0600

    Update setUsers to target users array in response from fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,42 @@
 import React, { useState, useEffect } from 'react'
-import { fakeData } from '../../data'
+// import { fakeData } from '../../data'
 import { Switch, Route } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Home from '../Home/Home'
 import './App.scss'
 import Dashboard from '../Dashboard/Dashboard'
-import Context from '../../Context'
+import UsersContext from '../../Context/UsersContext'
+import ErrorContext from '../../Context/ErrorContext'
 import { getUsers } from '../../utilities/apiCalls'
 
 export default function App() {
 
   const [users, setUsers] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {  
     const fetchUsers = async () => {
       try {
         const response = await getUsers()
-        console.log(response)
-        return response
+        setUsers(response.data)
       } catch (err) {
-        console.log(err) //set context state of error here
+        setError(err)
       }
     }
-    setUsers(fetchUsers())
+    fetchUsers()
   }, [])
 
   return (
-    <Context.Provider value={users}>
-      <main>
-          <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/dashboard' component={Dashboard} />
-        </Switch>
-      </main>
-    </Context.Provider>
+    <ErrorContext.Provider value={error}>
+      <UsersContext.Provider value={users}>
+        <main>
+            <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/dashboard' component={Dashboard} />
+          </Switch>
+        </main>
+      </UsersContext.Provider>
+    </ErrorContext.Provider>
   )
 }
