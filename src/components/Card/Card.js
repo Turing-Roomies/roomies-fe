4f1d3569f27c1d09840fa24f1e3d3d4cd2c85385
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-13 18:08:17 -0400

    Add conditional render to button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,30 +9,29 @@ const { name, location, gender, age } = user.attributes
 const [request, setRequest] = useState(false)
 const cursor = {cursor: 'pointer'}
 
-
   const changeRequest = () => {
     setRequest(true)
   }
   
-  return (
-    <article className='card'>
-      <div className='card-name'>
-        <h1>{name},</h1>
-        <h3>{age}</h3>
-      </div>
-      <div className='card-info'>
-        <div className='location-container'>
-          <img className='location-icon' src={locationIcon} alt='location icon'/>
-          <p>{location.city}, {location.state}</p>
-        </div>
-        <div className='gender-container'>
-          <img className='user-icon' src={userIcon} alt='user icon'/>
-          <p>{gender}</p>
-        </div>
-        {request ? <button className='req-contact' disabled={true}>Request sent!</button> 
-        : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button> 
-        } 
-      </div>
-    </article>
-  )
+    return (
+        <article className='card'>
+          <div className='card-name'>
+            <h1>{name},</h1>
+            <h3>{age}</h3>
+          </div>
+          <div className='card-info'>
+            <div className='location-container'>
+              <img className='location-icon' src={locationIcon} alt='location icon'/>
+              <p>{location.city}, {location.state}</p>
+            </div>
+            <div className='gender-container'>
+              <img className='user-icon' src={userIcon} alt='user icon'/>
+              <p>{gender}</p>
+            </div>
+          </div>
+          {request ? <button className='req-contact' disabled={true}>Request sent!</button> 
+          : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button> 
+          }
+        </article>
+    )
 }
