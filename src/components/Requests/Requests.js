40a722cbd74c4168a03b0da0f87b2efbd0bc29e7
Your Name <stoner.michann@gmail.com>
2021-07-13 14:30:36 -0600

    Create basic structure for Requests component

---
+++ src/components/Requests/Requests.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import './Requests.scss'
+
+export default function Requests() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
