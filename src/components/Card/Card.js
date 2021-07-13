de4338ea55d1453315ec32b0c370936bc6e29081
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-13 18:08:17 -0400

    Add conditional render to button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,27 @@
+<<<<<<< HEAD
 import React, { useContext } from 'react'
+=======
+import React, { useState } from 'react'
+>>>>>>> f28554e (Add conditional render to button)
 import './Card.scss'
 import locationIcon from '../../assets/location-icon-orange.png'
 import userIcon from '../../assets/user-icon-orange.png'
 import RequestRoomieContext from '../../Context/RequestRoomieContext'
 
 export default function Card({ user }) {
+<<<<<<< HEAD
 const { id, attributes: {name, location, gender, age } } = user
 const requestRoomie = useContext(RequestRoomieContext)
+=======
+const { name, location, gender, age } = user.attributes
+const [request, setRequest] = useState(false)
+const cursor = {cursor: 'pointer'}
+>>>>>>> f28554e (Add conditional render to button)
 
+  const changeRequest = () => {
+    setRequest(true)
+  }
+  
     return (
       <article className='card'>
         <div className='card-name'>
@@ -23,8 +37,16 @@ const requestRoomie = useContext(RequestRoomieContext)
             <img className='user-icon' src={userIcon} alt='user icon'/>
             <p>{gender}</p>
           </div>
+<<<<<<< HEAD
         </div>
         <button className='req-contact' onClick={ () => requestRoomie(id) }>Request Contact</button>
       </article>
   )
+=======
+          {request ? <button className='req-contact' disabled={true}>Request sent!</button> 
+          : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button> 
+          }
+        </article>
+    )
+>>>>>>> f28554e (Add conditional render to button)
 }
