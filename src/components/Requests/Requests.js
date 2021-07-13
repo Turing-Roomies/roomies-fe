73ccf4bb36b30251b6d322156f298ec3ca00ce74
Your Name <stoner.michann@gmail.com>
2021-07-13 14:38:54 -0600

    Import users to Requests component
    Co-authored-by: Dustin H <Thee-Dust@users.noreply.github.com>

--- src/components/Requests/Requests.js
+++ src/components/Requests/Requests.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import Context from '../../Context'
 import './Requests.scss'
+import Card from '../Card/Card'
 
 export default function Requests() {
+  const users = useContext(Context)
   return (
     <div>
+      <Card />
       
     </div>
   )
