ae57d65ca04e9f37ea7f9bb74f137bd47a8c8834
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-17 19:07:21 -0400

    fix spacing on jsx

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,25 +13,25 @@ const cursor = {cursor: 'pointer'}
     setRequest(true)
   }
   
-    return (
-      <article className='card'>
-        <div className='card-name'>
-          <h1>{name},</h1>
-          <h3>{age}</h3>
+  return (
+    <article className='card'>
+      <div className='card-name'>
+        <h1>{name},</h1>
+        <h3>{age}</h3>
+      </div>
+      <div className='card-info'>
+        <div className='location-container'>
+          <img className='location-icon' src={locationIcon} alt='location icon'/>
+          <p>{location.city}, {location.state}</p>
         </div>
-        <div className='card-info'>
-          <div className='location-container'>
-            <img className='location-icon' src={locationIcon} alt='location icon'/>
-            <p>{location.city}, {location.state}</p>
-          </div>
-          <div className='gender-container'>
-            <img className='user-icon' src={userIcon} alt='user icon'/>
-            <p>{gender}</p>
-          </div>
-            {request ? <button className='req-contact' disabled={true}>Request sent!</button> 
-            : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button> 
-            }
+        <div className='gender-container'>
+          <img className='user-icon' src={userIcon} alt='user icon'/>
+          <p>{gender}</p>
         </div>
-      </article>
-    )
+        {request ? <button className='req-contact' disabled={true}>Request sent!</button> 
+        : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button> 
+        }
+      </div>
+    </article>
+  )
 }
