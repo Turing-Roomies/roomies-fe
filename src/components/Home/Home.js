edd927cc620cd95dde3309803360936745411acb
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-16 20:09:20 -0400

    Import Login inside Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.scss'
+import Login from '../Login/Login'
 
 export default function Home() {
   
   return (
     <section className='welcome-section'>
+      <Login/>
       <div className='welcome-container' >
         <h2 className='welcome-message'>Find your<br></br>perfect<br></br> match.</h2>
       </div>
