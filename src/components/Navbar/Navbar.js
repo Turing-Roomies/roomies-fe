ad1130dbe0f7787e4176273055253c36d233d81a
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-16 23:08:01 -0400

    Create new navlink that navigates back to landing page & logs user out

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,23 @@ import React from "react"
 import { NavLink, Link } from "react-router-dom"
 import "./Navbar.scss"
 
-export default function Navbar() {
+export default function Navbar({ setCurrentUser }) {
   const linkStyle = { textDecoration: "none", color: "black" }
   const activeStyle = { color: "#157A6E" }
+
+  const logout = () => {
+    setCurrentUser(null)
+  }
+
   return (
     <header>
       <nav>
       <Link to='/' title='Roomies' style={linkStyle}><h1 className='page-header'>Roomies</h1></Link>
       <div>
         <ul className='nav-links'>
+          <li>
+            <NavLink to='/' title='Logout' onClick={logout} style={linkStyle} >Logout</NavLink>
+          </li>
           <li>
             <NavLink to='/dashboard' title='Dashboard' style={linkStyle} activeStyle={activeStyle}>Dashboard</NavLink>
           </li>
