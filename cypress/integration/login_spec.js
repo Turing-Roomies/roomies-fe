a9f9c6425e0d9ea258cdd055566427e950eb65aa
Your Name <stoner.michann@gmail.com>
2021-07-17 15:06:12 -0600

    Import icon and add to welcome header

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -5,7 +5,7 @@ describe('Home', () => {
 
     it('Should display an error if a user\'s credentials aren\'t found', () => {
         cy.logInWrongUser()
-          .get('h1').should('contain', 'Could not find login credentials! Please create an account or try again!')
+          .get('p').should('contain', 'Could not find login credentials. Please create an account or try again!')
           .get('.nav-links > li').should('have.length', '1')
           .get('.nav-links > li').eq(0).should('contain', 'Dashboard')
           .get('input[name=userName]').should('have.value', '')
@@ -14,7 +14,7 @@ describe('Home', () => {
 
     it('Should display a welcome message if a user\'s credentials are found', () => {
         cy.logInHarrison()
-          .get('h1').should('contain', 'Welcome, Harrison!')
+          .get('h3').should('contain', 'Welcome, Harrison!')
           .get('.nav-links > li').should('have.length', '2')
           .get('.nav-links > li').eq(0).should('contain', 'Logout')
           .get('.nav-links > li').eq(1).should('contain', 'Dashboard')
