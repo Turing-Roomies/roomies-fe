b5bf852df6895eec6d8753c90e55f0d6f5211b97
Your Name <stoner.michann@gmail.com>
2021-07-17 16:05:28 -0600

    Add section and header to dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react"
 import Card from "../Card/Card"
 import "./Dashboard.scss"
 import UsersContext from "../../Context/UsersContext"
+import homeIcon from '../../assets/home.png'
 
 export default function Dashboard() {
   const users = useContext(UsersContext)
@@ -9,5 +10,11 @@ export default function Dashboard() {
     return <Card user={user} key={user.id} />
   })
 
-  return <div className="cards-container">{cards}</div>
+  return <section className='card-section'>
+            <h3 className='roomies-header'>Available Roomies <img className='home-icon'src={homeIcon} alt='home icon'/></h3>
+            <div className="cards-container">
+              {cards}
+            </div>
+          </section>
+    
 }
