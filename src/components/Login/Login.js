08cb9a8a27024aa98ebfff2c7fe1e82a75b8666e
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-16 20:21:15 -0400

    Create inputs with submit button

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
 export default function Login() {
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
         <div>
-            <h1>TESTING LOGIN RENDER</h1>
+            <form>
+                <div>
+                    <label>User Name
+                        <input 
+                        type='text'
+                        required
+                        />
+                    </label>
+                    <label>password
+                        <input 
+                        type='text'
+                        required
+                        />
+                    </label>
+                    <button className='submit-button' onClick={event => handleSubmit(event)} type='submit'>Login</button>
+                </div>
+            </form>
         </div>
     )
 }
