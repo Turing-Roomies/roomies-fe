2eebec24f5cba9c188ee4f87cf149da8c2ab0196
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-07-19 13:12:17 -0400

    Conditionally render button if currentUser truthy

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,13 @@ import './Card.scss'
 import locationIcon from '../../assets/location-icon-orange.png'
 import userIcon from '../../assets/user-icon-orange.png'
 import UsersContext from "../../Context/UsersContext"
-import RequestRoomieContext from '../../Context/RequestRoomieContext'
+// import RequestRoomieContext from '../../Context/RequestRoomieContext'
 
 export default function Card({ user }) {
 const { id, attributes: {name, location, gender, age } } = user
 const [request, setRequest] = useState(false)
 const cursor = {cursor: 'pointer'}
 const { currentUser } = useContext(UsersContext)
-const requestRoomie = useContext(RequestRoomieContext)
 
   const changeRequest = () => {
     requestRoomie(currentUser.id, id)
