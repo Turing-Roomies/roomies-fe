fa801253e685b9cf64a3cf061b8f23203f85cfff
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-20 17:16:26 -0400

    Add function for delete request in button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,30 +3,38 @@ import UsersContext from "../../Context/UsersContext"
 import RequestRoomieContext from '../../Context/RequestRoomieContext'
 
 export default function Button({ id, email }) {
-const { currentUser } = useContext(UsersContext)
-const requestRoomie = useContext(RequestRoomieContext)
+  const { currentUser } = useContext(UsersContext)
+  const {requestRoomie, deleteRoomie } = useContext(RequestRoomieContext)
 
-const cursor = {cursor: 'pointer'}
+  const cursor = {cursor: 'pointer'}
 
   const changeRequest = (query) => {
     const reqBody = {
       "requestor_id": currentUser.id,
       "receiver_id": id
     }
-    requestRoomie(query, reqBody)
+    requestRoomie(reqBody, query)
+  }
+
+  const removeRequest = () => {
+    const remBody = {
+      "requestor_id": id,
+      "receiver_id": currentUser.id
+    }
+    deleteRoomie(remBody)
   }
 
   return (
     <div>
       {!currentUser ? null :
-      !!currentUser.attributes.roomies.find(user => user.roomie_a_id === Number(id)) ? <div>{email}</div> :
+      !!currentUser.attributes.roomies.find(user => user.receiver_id === Number(id) || user.requestor_id === Number(id)) ? <div>{email}</div> :
       !!currentUser.attributes.roomie_requests_sent.find(user => user.receiver_id === Number(id)) ? <div>Contact Sent!</div> :
       !!currentUser.attributes.roomie_requests_received.find(user => user.requestor_id === Number(id)) ?
         <div>
-          <button onClick={changeRequest('roomies')}>Accept</button>
-          <button onClick={changeRequest('roomie_requests/:id')}>Decline</button>
+          <button onClick={() => changeRequest('roomies')}>Accept</button>
+          <button onClick={removeRequest}>Decline</button>
         </div>
-        : <button className='req-contact' onClick={changeRequest('roomie_requests')} style={cursor} >Request Contact</button>}
+        : <button className='req-contact' onClick={() => changeRequest('roomie_requests')} style={cursor} >Request Contact</button>}
     </div>
   )
 }
