43daf533ce64a3925bbee576d5ae3a2de9134a51
Your Name <stoner.michann@gmail.com>
2021-07-19 22:50:06 -0600

    Write beforeEach and first two tests for RoomieRequests

---
+++ cypress/integration/roomie_request_spec.js
@@ -0,0 +1,22 @@
+describe('Roomie Requests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.fetchRoomies()
+    cy.logInHarrison()
+  })
+
+  it('Should navigate to Roomie Requests when link is clicked', () => {
+    cy.get('.nav-links > div > li').eq(1).should('contain', 'Roomie Requests')
+      .click()
+  })
+
+  it('Should display requests sent if the user has requested contact', () => {
+    cy.get('.nav-links > div > li').eq(1).click()
+      .get('div > article').should('have.length', 2)
+      .get('.card').eq(0).should('contain', 'Wyatt','Denver, CO', 'male', '30')
+      .get('.card > button').should('be.visible')
+      .get('.card').eq(1).should('contain', 'Sarah', 'Denver, CO', 'female', '33')
+      .get('.card > button').should('be.visible')
+
+  })
+})
