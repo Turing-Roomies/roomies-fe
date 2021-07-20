f5256d66f860883fa65b26ea1f2bd099ab6af76f
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-20 12:05:39 -0400

    Add functionality to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,28 @@
 import React, { useContext } from 'react'
 import UsersContext from "../../Context/UsersContext"
+import RequestRoomieContext from '../../Context/RequestRoomieContext'
 
-export default function Button() {
+export default function Button({ id, email }) {
 const { currentUser } = useContext(UsersContext)
+const requestRoomie = useContext(RequestRoomieContext)
+
+const cursor = {cursor: 'pointer'}
+
+  const changeRequest = () => {
+    requestRoomie(currentUser.id, id)
+  }
 
   return (
     <div>
-      
+      {!currentUser ? null :
+      !!currentUser.attributes.roomies.find(user => user.roomie_a_id === Number(id)) ? <div>{email}</div> :
+      !!currentUser.attributes.roomie_requests_sent.find(user => user.receiver_id === Number(id)) ? <div>Contact Sent!</div> :
+      !!currentUser.attributes.roomie_requests_received.find(user => user.requestor_id === Number(id)) ?
+        <div>
+          <button>Accept</button>
+          <button>Decline</button>
+        </div>
+        : <button className='req-contact' onClick={changeRequest} style={cursor} >Request Contact</button>}
     </div>
   )
 }
