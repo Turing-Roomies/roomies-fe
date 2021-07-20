a41b5b36e9527c1f230c4755fe54f264061ba1d3
Dustin-Har <harbaughdustin@yahoo.com>
2021-07-20 11:27:04 -0400

    Add Burron component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import UsersContext from "../../Context/UsersContext"
+
+export default function Button() {
+const { currentUser } = useContext(UsersContext)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
