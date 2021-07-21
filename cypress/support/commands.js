b05537f312f6f6048a66a3581847aad594300900
Your Name <stoner.michann@gmail.com>
2021-07-20 19:15:31 -0600

    Add POST to commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,115 +15,72 @@ Cypress.Commands.add('logInHarrison', () => {
       .get('.submit-button').click()
 })
 
-Cypress.Commands.add('fetchRoomies', () => {
-    cy.intercept('https://turing-roomies-be.herokuapp.com/api/v1/users',
+
+Cypress.Commands.add("fetchHarrison", () => {
+  cy.intercept( 'POST', 'https://turing-roomies-be.herokuapp.com/api/v1/sessions',
     {
-        "data": [{
-            "type": "users",
-            "id": "1",
-            "attributes": {
-                "email": "whatever@example.com",
-                "name": "Harrison",
-                "location": {
-                    "city": "Denver",
-                    "state": "CO"
-                },
-                "gender": "male",
-                "age": 26,
-                "roomies": [{
-                    "name": "Wyatt",
-                    "location": "Denver",
-                }]
-            }
+      statusCode: 200,
+      body: {
+          "data": {
+         "type": "users",
+        "id": "1",
+        "attributes": {
+          "email": "harrison@email.com",
+          "name": "Harrison",
+          "location": {
+            "city": "Denver",
+            "state": "CO",
         },
-        {
-            "type": "users",
-            "id": "2",
-            "attributes": {
-                "email": "someother@example.com",
-                "name": "Wyatt",
-                "location": {
-                    "city": "Denver",
-                    "state": "CO"
-                },
-                "gender": "male",
-                "age": 30,
-                "roomies": [
-                    {
-                        "name": "Harrison",
-                        "location": "Denver",
-                    },
-                    {
-                        "name": "Michann",
-                        "location": "Denver"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "users",
-            "id": "3",
-            "attributes": {
-                "email": "emailme@example.com",
-                "name": "Andrew",
-                "location": {
-                    "city": "West Palm Beach",
-                    "state": "FL"
-                },
-                "gender": "male",
-                "age": 25,
-                "roomies": [
-                    {
-                        "name": "Dustin",
-                        "location": "Orlando",
-                    }
-                ]
-            }
-        },
-        {
-            "type": "users",
-            "id": "4",
-            "attributes": {
-                "email": "email@example.com",
-                "name": "Sarah",
-                "location": {
-                    "city": "Denver",
-                    "state": "CO"
-                },
-                "gender": "female",
-                "age": 33,
-                "roomies": [
-                    {
-                        "name": "Michann",
-                        "location": "Denver",
-                    },
-                ]
-            }
-        },
-        {
-            "type": "users",
-            "id": "5",
-            "attributes": {
-                "email": "somebodysemail@example.com",
-                "name": "Michann",
-                "location": {
-                    "city": "Denver",
-                    "state": "CO"
-                },
-                "gender": "female",
-                "age": 27,
-                "roomies": [
-                    {
-                        "name": "Sarah",
-                        "location": "Denver",
-                    },
-                    {
-                        "name": "Wyatt",
-                        "location": "Denver",
-                    }
-                ]
-            }
+        "gender": "male",
+        "age": 26,
+        "roomie_requests_sent": [],
+        "roomie_requests_received": [],
+        "roomies": []
         }
-        ]
-    })
-})
+        }
+      }
+  })
+})  
+
+
+Cypress.Commands.add("fetchRoomies", () => {
+  cy.intercept("https://turing-roomies-be.herokuapp.com/api/v1/users", {
+    "data": [{
+        "type": "users",
+        "id": "1",
+        "attributes": {
+            "email": "harrison@email.com",
+            "name": "Harrison",
+            "location": {
+                "city": "Denver",
+                "state": "CO"
+            },
+            "gender": "male",
+            "age": 26,
+            "roomie_requests_sent": [],
+            "roomie_requests_received": [],
+            "roomies": []
+        }
+    },
+    {
+        "type": "users",
+        "id": "2",
+        "attributes": {
+            "email": "someother@example.com",
+            "name": "Wyatt",
+            "location": {
+                "city": "Denver",
+                "state": "CO"
+            },
+            "gender": "male",
+            "age": 30,
+            "roomie_requests_sent": [],
+            "roomie_requests_received": [],
+            "roomies": []
+        }
+    }]
+
+  });
+}); 
+
+
