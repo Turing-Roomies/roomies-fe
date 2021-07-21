9659a31344dc2b3176bbf7c9d57fa714a277de84
Your Name <stoner.michann@gmail.com>
2021-07-20 19:13:13 -0600

    Update tests for login

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -15,16 +15,14 @@ describe('Home', () => {
     it('Should display a welcome message if a user\'s credentials are found', () => {
         cy.logInHarrison()
           .get('h3').should('contain', 'Welcome, Harrison Blake!')
+          .get('form').should('not.exist')
+    })
+
+    it('Should display additional nav-links if a user is logged in', () => {
+        cy.logInHarrison()
           .get('.nav-links > div > li').should('have.length', '2')
           .get('.nav-links > div > li').eq(0).should('contain', 'Logout')
           .get('.nav-links > div > li').eq(1).should('contain', 'Roomie Requests')
           .get('.nav-links > li').eq(0).should('contain', 'Dashboard')
-          .get('form').should('not.exist')
     })
-
-    // it('Should display Request Contact button if user is logged in', () => {
-    //     cy.logInHarrison()
-    //       .get('.nav-links > li > a').click()
-    //       .get('.card').eq(2).get('req-contact').should('be.visible')
-    // })
 })
