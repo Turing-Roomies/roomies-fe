c803df29013d55d0ec508c05ce39621bd7036c71
Your Name <stoner.michann@gmail.com>
2021-07-20 21:15:01 -0600

    Updated and added new tests for roomie requests

--- cypress/integration/roomie_request_spec.js
+++ cypress/integration/roomie_request_spec.js
@@ -1,29 +1,30 @@
 describe('Roomie Requests', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.fetchHarrison()
     cy.fetchRoomies()
+    cy.visit("http://localhost:3000");
     cy.logInHarrison()
   })
 
   it('Should navigate to Roomie Requests when link is clicked', () => {
-    cy.get(".nav-links > div > li")
-      .eq(1)
-      .should('contain', 'Roomie Requests')
-      .click()
-      .url()
-      .should('eq', 'http://localhost:3000/requests')
+    cy.get('.roomie-requests').should('contain', 'Roomie Requests').click()
+      .url().should('eq', 'http://localhost:3000/requests')
   })
+
+it("Should display a message if the user has not requested any roomies", () => {
+    cy.get('.roomie-requests').click()
+    cy.get('.no-roomies-msg').should('contain', 'Sorry, you don\'t have any roomies yet!')
+});
 
   it('Should display requests sent if the user has requested contact', () => {
-    cy.get('.nav-links > div > li').eq(1).click()
-      .get('div > article').should('have.length', 2)
-      .get('.card').eq(0).should('contain', 'Wyatt','Denver, CO', 'male', '30')
-      .get('.card > button').should('be.visible')
-      .get('.card').eq(1).should('contain', 'Sarah', 'Denver, CO', 'female', '33')
-      .get('.card > button').should('be.visible')
-  })
+    cy.postRoomieRequest()
+      .get(".dashboard").should("contain", "Dashboard").click()
+      .get("div > article").should("have.length", 1)
+      .get(".card").eq(0).should("contain", "Wyatt", "Denver, CO", "male", "30")
+      .get(".req-contact").should("be.visible").click()
+      .get('.contact-message').should('contain', 'Contact Sent!')
+      .get('.roomie-requests').should('contain', 'Roomie Requests').click()
+  
+    })
 
-  it('Should display a message if the user has not requested any roomies', () => {
-    
-  })
 })
